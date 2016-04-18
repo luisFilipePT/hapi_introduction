5777b7b5a4cca1e492f4c9c5926140979527b5dd
Luis Filipe <luis.fgbo@gmail.com>
2016-04-18 22:26:30 +0100

    Initial server setup

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+// Create a new server instance
+const server = new Hapi.Server();
+
+// Set the connection 
+server.connection({ 
+    host: 'localhost', 
+    port: 3000 
+});
+
+// Start the server
+server.start((err) => {
+    console.log('Server running at:', server.info.uri);
+});
